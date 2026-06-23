9338c7f746bd287fae808086d0cff8394b50d435
LT8wrage <lt80029@gmail.com>
2026-06-23 11:00:57 +0100

    added few blocks

--- src/main/java/net/lt8wrage/ragemod/RageMod.java
+++ src/main/java/net/lt8wrage/ragemod/RageMod.java
@@ -66,6 +66,7 @@ public class RageMod {
         }
         if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
             event.accept(ModBlocks.ENDERITE_DEBRIS);
+            event.accept(ModBlocks.ENDERITE_BLOCK);
         }
     }
 
